303dbd642cb6bd343fe098e3da9049bf4f1d33a9
Dave <psychostasia1@gmail.com>
2022-12-18 16:05:15 -0600

    finished project

--- hangman.rb
+++ hangman.rb
@@ -1,7 +1,14 @@
 class Hangman
   def initialize
-    @letters = ('a'...'z').to_a
+    @letters = ('a'..'z').to_a
     @word = words.sample
+    @guesses = 7
+    @correct_guesses = []
+    @word_teaser = ""
+
+    @word.first.size.times do
+      @word_teaser += "_ "
+    end
   end
   
   def words
@@ -14,22 +21,61 @@ class Hangman
     ]
   end
 
-  def print_teaser 
-    word_teaser = ""
-    @word.first.size.times do
-      word_teaser += "_ "
+  def print_teaser last_guess = nil
+
+    update_teaser(last_guess) unless last_guess.nil?
+    puts @word_teaser
+  end
+
+  def update_teaser last_guess
+    new_teaser = @word_teaser.split
+
+    new_teaser.each_with_index do |letter, index|
+
+      if  letter == '_' && @word.first[index] == last_guess
+        new_teaser[index] = last_guess
+      end
     end
-    puts word_teaser
+    @word_teaser = new_teaser.join(' ')
   end
 
   def make_guess
+    if @guesses > 0
     puts "Enter a letter"
     guess = gets.chomp
+
+    good_guess = @word.first.include? guess
+
+
+    if guess == "exit"
+      puts "Thanks for playing"
+    elsif good_guess
+      puts "Good guess!"
+
+      # @correct_guesses << guess
+      # @letters.delete guess
+
+      print_teaser guess
+
+      if @word.first == @word_teaser.split.join
+        puts "You win!"
+      else
+        make_guess
+      end
+    else
+      @guesses -= 1
+      puts "Sorry... you have #{ @guesses } guesses left, try again."
+      make_guess
+    end
+  else
+    puts "Game Over"
+  end
   end
   
 
   def begin
     puts "New game started... your word is #{ @word.first.size } character long"
+    puts "To exit game type 'exit'"
     print_teaser
     puts "Clue is #{ @word.last }"
 
